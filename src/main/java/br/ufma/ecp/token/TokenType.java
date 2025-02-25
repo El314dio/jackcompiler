6d52e819a9a67184d59b24fa986768ac983898d9
El314dio <elnetto001@gmail.com>
2025-02-25 00:16:43 -0300

    Adicionando novos termos ao TokenType

--- src/main/java/br/ufma/ecp/token/TokenType.java
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -11,19 +11,37 @@ public enum TokenType {
      STRING,
 
 
-     IDENT,
+     IDENT, EQ, SEMICOLON,
+
+
+     PRINT,
+
+
+    // keywords
+    WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+    METHOD,FIELD,STATIC,VAR,INT,
+    CHAR,BOOLEAN,VOID,TRUE,FALSE,
+    NULL,THIS,LET,DO,IF,ELSE, RETURN,
+
+
+     //simbolos
+      LPAREN,RPAREN,
+         LBRACE, RBRACE,
+         LBRACKET,RBRACKET,
+
+     COMMA, DOT,
+
+     ASTERISK, SLASH,
+
+     AND, OR, NOT,
+
+     LT, GT, ILLEGAL,
+     
+     EOF;
 
  
      // keywords
-     METHOD,
-     WHILE,
-     IF,
-     CLASS,
-     CONSTRUCTOR,
 
-     EOF,
-
-     ILLEGAL;
 
      static public boolean isSymbol (char c) {
         String symbols = "{}()[].,;+-*/&|<>=~";
